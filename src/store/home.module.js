e9b88e55dd73bd26b8b082f37a54fadfdb45070e
Kamil <stacho688@gmail.com>
2020-04-17 17:00:31 +0200

    Progress

---
+++ src/store/home.module.js
@@ -0,0 +1,17 @@
+const state = {
+    userLoggedIn: false
+}
+
+const getters = {
+    isUserLoggedIn(state) {
+        return state.userLoggedIn
+    }
+}
+
+const actions = {
+    [ACTION_NAME]()
+}
+
+export default {
+    getters
+}
