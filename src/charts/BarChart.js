5398290ac3a8235c31f46b5bbac9a98d21ce2329
Kamil Wieniecki <wienio96@gmail.com>
2020-05-02 16:59:11 +0200

    Charts implementation

---
+++ src/charts/BarChart.js
@@ -0,0 +1,33 @@
+import { Bar, mixins } from 'vue-chartjs'
+
+const { reactiveProp } = mixins
+
+export default {
+  extends: Bar,
+  mixins: [reactiveProp],
+  props: ['options'],
+  data: () => ({
+    chartData: {
+      labels: ['Biceps', 'Triceps'],
+      datasets: [{
+        label: 'Trainings',
+        backgroundColor: '#f87979',
+        data: [10, 15]
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  }),
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  }
+}
--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -1,11 +1,66 @@
 <template>
-  <md-content class="md-primary">Progress</md-content>
+  <div class="flex-container">
+    <!-- <md-content class="md-primary">Progress</md-content> -->
+    <div class="column">
+      <bar-chart :chart-data="barChartData" />
+    </div>
+    <div class="column">
+      <doughnut-chart />
+    </div>
+    <div class="column"></div>
+    <div class="column"></div>
+  </div>
 </template>
 
 <script>
+import BarChart from "@/charts/BarChart";
+import DoughnutChart from "@/charts/DoughnutChart";
+import FirebaseService from "@/common/Firebase.service";
 
 export default {
-  name: "training-progress"
-
+  components: {
+    BarChart,
+    DoughnutChart
+  },
+  data() {
+    return {
+      barChartData: null,
+      doughnutChartData: null
+    };
+  },
+  methods: {
+    getBarChartData() {
+      let result = FirebaseService.getExercises(2);
+      console.log(result);
+      this.barChartData = {
+        // FirebaseService.getExercises(1);
+      };
+    }
+  },
+  mounted() {
+    this.getBarChartData();
+  }
 };
 </script>
+
+<style scoped>
+.flex-container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.column {
+  width: 45%;
+  margin: 2%;
+  height: 420px;
+}
+
+@media only screen and (max-width: 768px) {
+  .column {
+    width: 75%;
+    height: inherit;
+  }
+}
+</style>
